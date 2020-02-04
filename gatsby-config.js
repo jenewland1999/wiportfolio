70ddf2657cf82c8540b979eaf0f1aa5a95efea42
jenewland1999 <github@jenewland.me.uk>
2020-02-04 18:16:00 +0000

    ✨ Implement Gatsby boilerplate and plugins
    Add all the necessary boilerplate for the project

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+module.exports = {
+    siteMetadata: {
+        title: `W.I.Portfolio`,
+        description: `My forever work in progress portfolio website.`,
+        author: `@jenewland1999`,
+        siteUrl: `https://jenewland.me.uk/`
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-react-helmet`
+        },
+        {
+            resolve: `gatsby-plugin-sharp`
+        },
+        {
+            resolve: `gatsby-transformer-sharp`
+        },
+        {
+            resolve: `gatsby-plugin-sitemap`
+        },
+        {
+            resolve: `gatsby-plugin-robots-txt`
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `W.I.Portfolio`,
+                short_name: `W.I.Portfolio`,
+                description: `My forever work in progress portfolio website.`,
+                lang: `en`,
+                start_url: `/`,
+                background_color: `#6a1b9a`,
+                theme_color: `#6a1b9a`,
+                display: `standalone`
+                // icon: ``, // This path is relative to the root of the site.
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`
+            }
+        },
+        {
+            resolve: `gatsby-plugin-web-font-loader`,
+            options: {
+                typekit: {
+                    id: `fvl5vuj`
+                }
+            }
+        }
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ]
+};
