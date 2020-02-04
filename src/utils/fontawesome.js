91698143f499498be2a575bda09d6995259ecfc4
jenewland1999 <github@jenewland.me.uk>
2020-02-04 18:54:28 +0000

    ✨ Create FontAwesome library for project

---
+++ src/utils/fontawesome.js
@@ -0,0 +1,16 @@
+import { library } from "@fortawesome/fontawesome-svg-core"
+import {
+    faHome,
+    faBriefcase,
+    faPencilRuler,
+    faUser,
+    faEnvelope,
+    faEllipsisH,
+} from "@fortawesome/free-solid-svg-icons"
+import {
+    fabGithubSquare,
+    fabLinkedin,
+    fabTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons"
+
+library.add(faHome, faBriefcase, faPencilRuler, faUser, faEnvelope, faEllipsisH)
