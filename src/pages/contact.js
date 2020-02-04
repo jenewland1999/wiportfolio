ec209bd9f3276d3c6f49b14534324cb0ac85af4f
jenewland1999 <github@jenewland.me.uk>
2020-02-04 18:53:43 +0000

    ✨ Add basic routes/pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const ContactPage = () => (
     <Layout>
-        <SEO title="Homepage" />
-        <h1>Homepage</h1>
+        <SEO title="Contact" />
+        <h1>Contact</h1>
     </Layout>
 )
 
-export default IndexPage
+export default ContactPage
