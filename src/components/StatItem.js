4fd0990e253b9e5717139fd3ddf249ab0e77c540
jenewland1999 <github@jenewland.me.uk>
2021-05-20 23:36:17 +0100

    💚 Remove dompurify to fix build

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,15 +1,10 @@
 import * as React from "react";
-import DOMPurify from "dompurify";
-
-const createMarkup = (text) => {
-  return { __html: DOMPurify.sanitize(text) };
-};
 
 const StatItem = ({ icon, text }) => {
   return (
     <div>
       <dt className="text-4xl text-purple-600">{icon}</dt>
-      <dd dangerouslySetInnerHTML={createMarkup(text)}></dd>
+      <dd dangerouslySetInnerHTML={{ __html: text }}></dd>
     </div>
   );
 };
