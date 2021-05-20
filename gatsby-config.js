593b0dcc020d6256ac25aa724fa5d222402e8aa0
jenewland1999 <github@jenewland.me.uk>
2021-05-20 15:02:34 +0100

    ⬆️ Upgrade gatsby-plugin-sharp to v3.5.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,10 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: { failOnError: false },
+    },
     `gatsby-plugin-postcss`,
     `gatsby-plugin-svgr`,
     {
