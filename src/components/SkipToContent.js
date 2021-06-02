cf5e95440471fa72d92bb61ff7f210f701f06ff5
jenewland1999 <github@jenewland.me.uk>
2021-06-02 09:38:58 +0100

    🎨 Refactor the SkipToContent component

--- src/components/SkipToContent.js
+++ src/components/SkipToContent.js
@@ -1,14 +1,31 @@
 import * as React from "react";
+import ctl from "@netlify/classnames-template-literals";
+import ButtonAnchor from "./ButtonAnchor";
+
+const skipToContentClasses = ctl(`
+  sr-only
+  focus:not-sr-only
+  focus:mx-4
+  focus:my-6
+  focus:px-6
+  focus:py-3
+  lg:transform
+  lg:focus:m-0
+  lg:focus:top-6
+  lg:focus:left-2/4
+  lg:focus:absolute
+  lg:focus:-translate-x-2/4
+`);
 
 const SkipToContent = ({ contentId, label, icon }) => {
   return (
-    <a
+    <ButtonAnchor
+      className={skipToContentClasses}
       href={contentId}
-      className="sr-only inline-flex items-center justify-center top-6 left-2/4 outline-none ring-2 ring-black ring-offset-2 rounded-md shadow-sm transform -translate-x-2/4 bg-black font-medium text-center text-white whitespace-nowrap focus:absolute focus:px-6 focus:py-3 focus:not-sr-only"
-    >
-      {icon && icon}
-      <span className={icon ? "ml-2" : ""}>{label}</span>
-    </a>
+      icon={icon}
+      text={label}
+      variant="secondary"
+    />
   );
 };
 
